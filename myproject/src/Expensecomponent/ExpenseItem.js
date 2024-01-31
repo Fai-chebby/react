1846858c6175360752df2a792b8d51092e4098ce
Fai-chebby <chebetf034@gmail.com>
2024-01-31 12:51:11 +0300

    💯

--- myproject/src/Expensecomponent/ExpenseItem.js
+++ myproject/src/Expensecomponent/ExpenseItem.js
@@ -1,11 +1,14 @@
 import "./ExpenseItem.css";
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 8);
+  const expenseTitle = "Car Insuarance";
+  const expenseAmount = 294;
   return (
     <div className="expense-item">
-      <div>March 28th</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car insuarance</h2>
-        <div className="expense-item__price">$249</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
