93a1733e8a160e3d6ab0323b9d6f95dfb152c334
Fai-chebby <chebetf034@gmail.com>
2024-02-17 02:02:17 +0300

    👩‍💻

--- demo2/src/components/Navbar.js
+++ demo2/src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React,{useState} from "react";
 import {Link} from "react-router-dom";
+import {click} from "@testing-library/user-event/dist/click";
 
 function Navbar(){
+    const [click,setClick]=useState()
     return(
         <div>
 < nav className="navbar">
@@ -10,7 +12,7 @@ function Navbar(){
             BIKE<i className ="fab fa-typo3"/>
         </Link>
         <div className='menu-icon'>
-            
+            <i className={click? 'fas fa-time':"fas fa-bar"}/>
         </div>
     </div>
 </nav>
