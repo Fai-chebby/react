cd4c71f833f6052b2a6a5421f4f8d56b0be76e39
Fai-chebby <chebetf034@gmail.com>
2024-02-17 01:44:24 +0300

    👩‍💻

--- demo2/src/components/Navbar.js
+++ demo2/src/components/Navbar.js
@@ -1,10 +1,15 @@
 import React,{useState} from "react";
+import {Link} from "react-router-dom";
 
 function Navbar(){
     return(
         <div>
 < nav className="navbar">
-    <div className="navbar-container"></div>
+    <div className="navbar-container">
+        <Link to"/" className="navbar-logo">
+            BIKE
+        </Link>
+    </div>
 </nav>
         </div>
     )
