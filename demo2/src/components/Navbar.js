02288d0e0695022fd0c1559049915b1aeeba5097
Fai-chebby <chebetf034@gmail.com>
2024-02-17 01:58:20 +0300

    👩‍💻

--- demo2/src/components/Navbar.js
+++ demo2/src/components/Navbar.js
@@ -7,8 +7,11 @@ function Navbar(){
 < nav className="navbar">
     <div className="navbar-container">
         <Link to"/" className="navbar-logo">
-            BIKE
+            BIKE<i className ="fab fa-typo3"/>
         </Link>
+        <div className='menu-icon'>
+            
+        </div>
     </div>
 </nav>
         </div>
