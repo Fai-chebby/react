97b7c0b3c1ad325351cb3f160a8828cce2144423
Fai-chebby <chebetf034@gmail.com>
2024-02-22 00:15:45 +0300

    👩‍💻

---
+++ bikewebsite/src/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+const Navbar=()=>{
+    return(
+        <div className='nav'>
+            <div className='nav-logo'>Ducati</div>
+            <ul className='nav-menu'>
+                <li>Home</li>
+                <li>Explore</li>
+                <li>About</li>
+                <li>Contct</li>
+            </ul>
+        </div>
+    )
+}
