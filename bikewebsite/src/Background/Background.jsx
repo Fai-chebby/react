69b6eec79be930a12d702fe6fa10c9d435e74921
Fai-chebby <chebetf034@gmail.com>
2024-02-21 23:58:32 +0300

    👩‍💻

--- bikewebsite/src/Background/Background.jsx
+++ bikewebsite/src/Background/Background.jsx
@@ -1,5 +1,8 @@
 import './Background.css'
-
+import video1 from '../Assets/vedio1.mp4'
+import image1 from '../Assets/image1.jpg'
+import image2 from '../Assets/image2.jpg'
+import image3 from '../Assets/image3.jpg'
 const Background=( {playStatus,heroCount})=>{
     return(
         <div>
