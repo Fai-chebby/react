4a515117e22afc8c6ba3c071eb6d791a4221a78c
Fai-chebby <chebetf034@gmail.com>
2024-02-22 00:46:43 +0300

    👩‍💻

--- bikewebsite/src/Hero/Hero.jsx
+++ bikewebsite/src/Hero/Hero.jsx
@@ -1,8 +1,25 @@
 import './Hero.css'
-
+import arrow_btn from '../Assets/arrow_btn.png'
+import pause_icon from '../Assets/pause_icon.png'
+import play_icon from '../Assets/play_icon.png'
 const Hero=()=>{
-    return{
+    return(
+        <div className='hero'>
+            <div className='hero-text'>
+                <p>{heroData.text1}</p>
+                <p>{heroData.text2}</p>
+            </div>
+<div className='hero-explore'>
+    <p>Explore the features</p>
+    <img src={arrow_btn } alt={}/>
 
-    }
+</div>
+       <div className='hero-dot-play'>
+           
+       </div>
+
+        </div>
+
+    )
 }
 export default Hero
