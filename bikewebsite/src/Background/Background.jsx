688f409a498c62c02b784501296305b8ea05ad62
Fai-chebby <chebetf034@gmail.com>
2024-02-22 00:06:08 +0300

    👩‍💻

--- bikewebsite/src/Background/Background.jsx
+++ bikewebsite/src/Background/Background.jsx
@@ -4,11 +4,23 @@ import image1 from '../Assets/image1.jpg'
 import image2 from '../Assets/image2.jpg'
 import image3 from '../Assets/image3.jpg'
 const Background=( {playStatus,heroCount})=>{
-    return(
-        <div>
+    if (playStatus){
+       return(
+           <video className='background' autoPlay loop muted>
+               <source src={video1} type='vedio/mp4'/>
+           </video>
+       )
+    }
+    else if (heroCount===0){
+        return <img src={image1} className='background' alt=''/>
+    }
+    else if (heroCount===1){
+        return <img src={image2} className='background' alt=''/>
+    }
+    else if (heroCount===2){
+        return <img src={image3} className='background' alt=''/>
+    }
 
-        </div>
-    )
 
 }
 export default Background
