e410262aaa076cbfe2d5bdb5d67d0be8c1b3856c
Fai-chebby <chebetf034@gmail.com>
2024-02-22 13:20:01 +0300

    Bikes website

--- bikewebsite/src/App.jsx
+++ bikewebsite/src/App.jsx
@@ -1,27 +1,37 @@
-import {useState} from "react";
+import { useEffect, useState } from "react";
 import Background from "./Background/Background";
 import Navbar from "./Navbar/Navbar";
 import Hero from "./Hero/Hero";
 
-const App=()=>{
-    let heroData=[
-        {text1:"Dive into", text2:"where you love"},
-        {text1:"Indulge", text2:"Your passion"},
-        {text1:"Give into", text2:"Your passion"},
-    ]
-    const [heroCount,setHeroCount]=useState(2);
-    const [playStatus,setPlayStatus]=useState(false);
-    return(
-        <div>
-<Background playStatus={playStatus} heroCount={heroCount}/>
-            <Navbar/>
-            <Hero setPlaySatus={setPlayStatus}
-                  HeroData={heroData[heroCount]}
-                  setHeroEvent={setHeroCount}
-                  playStatus={playStatus}
-            />
+const App = () => {
+    let heroData = [
+        { text1: "Dive into", text2: "where you love" },
+        { text1: "Indulge", text2: "Your passion" },
+        { text1: "Give into", text2: "Your passion" },
+    ];
 
+    const [heroCount, setHeroCount] = useState(0); // Initialize heroCount to 0 instead of 2
+    const [playStatus, setPlayStatus] = useState(false);
+
+    useEffect(() => {
+        setInterval(() => {
+            // Increment heroCount cyclically
+            setHeroCount((count) => (count + 1) );
+        }, 3000);
+    }, []); // Ensure this effect runs only once by providing an empty dependency array
+
+    return (
+        <div>
+            <Background playStatus={playStatus} heroCount={heroCount} />
+            <Navbar />
+            <Hero
+                setPlayStatus={setPlayStatus}
+                heroData={heroData[heroCount]} // Access correct heroData using heroCount
+                setHeroEvent={setHeroCount}
+                playStatus={playStatus}
+            />
         </div>
-    )
-}
-export default App
+    );
+};
+
+export default App;
