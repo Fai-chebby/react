efc020a27c76472ec84bd17a26e073bbfb70825e
Fai-chebby <chebetf034@gmail.com>
2024-02-25 15:45:23 +0300

    Navbar

--- navbar/src/Components/Navbar.jsx
+++ navbar/src/Components/Navbar.jsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
 import {Link, NavLink} from "react-router-dom"
 import "./Navbar.css"
 export const Navbar=()=>{
+    const [ menuOpen, setMenuOpen]=useState(false)
   return(
       <nav>
           <Link to="/" classname="title">Home</Link>
-          <div className="menu">
+          <div className="menu" onClick={()=>{ setMenuOpen(!menuOpen)}}>
               <span></span>
               <span></span>
               <span></span>
 
           </div>
-          <ul>
+          <ul className={ menuOpen?"open":""}>
               <li>
                   <NavLink to='/about'>About</NavLink>
               </li>
