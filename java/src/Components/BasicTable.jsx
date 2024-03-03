864bcf0d93b2863df02afb33f95f2d0dea068adb
Fai-chebby <chebetf034@gmail.com>
2024-03-03 23:25:05 +0300

    Tables

---
+++ java/src/Components/BasicTable.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {useTable} from "react-table/src";
+import MOCK_DATA from './MOCK_DATA.json'
+import {Columns} from "./Columns";
+
+export const BasicTable=()=>{
+    return(
+        <div>
+
+        </div>
+    )
+}
