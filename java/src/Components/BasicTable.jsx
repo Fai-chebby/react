16749170324e36fec1d35a6c82560da38b09d815
Fai-chebby <chebetf034@gmail.com>
2024-03-03 23:32:21 +0300

    Tables

--- java/src/Components/BasicTable.jsx
+++ java/src/Components/BasicTable.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React,{useMemo} from "react";
 import {useTable} from "react-table/src";
 import MOCK_DATA from './MOCK_DATA.json'
 import {Columns} from "./Columns";
 
 export const BasicTable=()=>{
+    const columns=useMemo(()=>Columns,[])
+    const data =  useMemo(()=> MOCK_DATA,[])
+    useTable({
+        columns:Columns,
+        data:MOCK_DATA
+    })
+
+
     return(
         <div>
 
