17e6e5d1cb0b58eb39e9e84a03e0ce6717886317
Fai-chebby <chebetf034@gmail.com>
2024-03-03 23:43:12 +0300

    Table

--- java/src/Components/BasicTable.jsx
+++ java/src/Components/BasicTable.jsx
@@ -6,15 +6,28 @@ import {Columns} from "./Columns";
 export const BasicTable=()=>{
     const columns=useMemo(()=>Columns,[])
     const data =  useMemo(()=> MOCK_DATA,[])
-    useTable({
-        columns:Columns,
-        data:MOCK_DATA
+     const tableInstance=useTable({
+        columns,
+        data
     })
 
 
     return(
         <div>
+<table>
+    <thead>
+    <tr>
+        <th>
 
+        </th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td></td>
+    </tr>
+    </tbody>
+</table>
         </div>
     )
 }
