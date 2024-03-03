0bea222fab70fbd9880f886390b8bdbc0aebe972
Fai-chebby <chebetf034@gmail.com>
2024-03-03 23:13:50 +0300

    Tables

---
+++ java/src/Components/Columns.jsx
@@ -0,0 +1,20 @@
+export const Columns=[
+    {
+        Header:'id',
+    },
+    {
+        Header:'First Name',
+    },
+    {
+        Header:'Last Name',
+    },
+    {
+        Header:'Date of Birth',
+    },
+    {
+        Header:'Gender',
+    },
+    {
+        Header:'Phone',
+    }
+]
