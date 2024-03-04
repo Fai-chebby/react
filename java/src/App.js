970e08c4c644c3a0fb5c93fecf9a2bb66ccff0ca
Fai-chebby <chebetf034@gmail.com>
2024-03-04 21:47:12 +0300

    Table

--- java/src/App.js
+++ java/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BasicTable} from "./Components/BasicTable";
 
 function App() {
   return (
     <div className="App">
-
+<BasicTable/>
     </div>
   );
 }
