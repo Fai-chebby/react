6743a4dc8fbc25b5f4a2d276f89649045ee68892
Fai-chebby <chebetf034@gmail.com>
2024-03-11 12:04:59 +0300

    dom

--- researchstudy/src/Components/Research.jsx
+++ researchstudy/src/Components/Research.jsx
@@ -1,11 +1,11 @@
  export  const Research=()=>{
     return(
         <div>
-            <div id="wrapper">
+            <div id="container">
                 <header>
                     <div id="page-banner">
-                        <h1 className="title">Bookorama</h1>
-                        <p>Books for Ninjas</p>
+                        <h1 className="title">ResearchStudy</h1>
+                        <p>Books to study</p>
                         <form id="search-books">
                             <input type="text" placeholder="Search books..."/>
                         </form>
@@ -15,19 +15,19 @@
                     <h2 className="title">Books to Read</h2>
                     <ul>
                         <li>
-                            <span className="name">Name of the Wind</span>
+                            <span className="name">How to Pray Effectively</span>
                             <span className="delete">delete</span>
                         </li>
                         <li>
-                            <span className="name">The Wise Man's Fear</span>
+                            <span className="name">The power-of-Tongues</span>
                             <span className="delete">delete</span>
                         </li>
                         <li>
-                            <span className="name">Kafka on the Shore</span>
+                            <span className="name">Your-Rights-in-Christ</span>
                             <span className="delete">delete</span>
                         </li>
                         <li>
-                            <span className="name">The Master and the Margarita</span>
+                            <span className="name">How to Make Your Faith Work</span>
                             <span className="delete">delete</span>
                         </li>
                     </ul>
