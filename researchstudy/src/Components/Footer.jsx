a72a3f13550d992c531baf680c7a71f811e5e113
Fai-chebby <chebetf034@gmail.com>
2024-03-14 23:47:13 +0300

    Footer

---
+++ researchstudy/src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './footer.css'
+const Footer=()=>{
+    return(
+        <div className="footer">
+            <div className="sb_footersection_padding">
+                <div className="sb_footer_link">
+                    <div className="sb_footer">
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
+export default Footer;
