7fd88982e2e40eba4c843b515c972ef72ce331b2
Fai-chebby <chebetf034@gmail.com>
2024-03-18 14:50:54 +0300

    flex

--- researchstudy/src/App.js
+++ researchstudy/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import {Research} from "./Components/Research";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <   div className="App">
     <Research/>
+<Footer/>
     </div>
   );
 }
--- researchstudy/src/Components/Footer.jsx
+++ researchstudy/src/Components/Footer.jsx
@@ -3,23 +3,15 @@ import './footer.css'
 const Footer=()=>{
     return(
         <div className="footer">
-            <div className="sb_footersection_padding">
-                <div className="sb_footer-links">
-                    <div className="sb_footer-links-div">
-<h4> For More Details</h4>
-                        <a href="contacts">
-                            <p>Contacts</p>
-                            <a href="Details">
-                                <p>Details</p>
-                                <a href="books">
-                                    <p>Books</p>
-                                </a>
+<div className="wrapper">
+    <div className="flex-container">
+        <div className="box one"></div>
+        <div className="box two"></div>
+        <div className="box three"></div>
 
-                    </div>
+    </div>
 
-                </div>
-            </div>
-
+</div>
         </div>
     )
 }
