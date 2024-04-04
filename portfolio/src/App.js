8883c8f52250ff9e4949f0ca399eadf26be0c2ce
Fai-chebby <chebetf034@gmail.com>
2024-04-04 22:26:02 +0300

    My portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import HeroSection from "./Data/Pages/HeroSection";
 
 function App() {
   return (
     <div className="App">
-
+<HeroSection/>
     </div>
   );
 }
--- portfolio/src/Data/Pages/HeroSection.jsx
+++ portfolio/src/Data/Pages/HeroSection.jsx
@@ -2,8 +2,9 @@ export default function HeroSection(){
     return(
         <div id="HeroSection" className="Hero--section">
          <div className="Hero--section--box">
-             <div>
-                 
+             <div className="Hero--section--content">
+                 <p className="title"> Hey 🤗 I'm Chebby</p>
+
              </div>
 
          </div>
