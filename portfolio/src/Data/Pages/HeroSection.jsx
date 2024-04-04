1de42a306dbacdfb6db1fce4e2d10453e27836ae
Fai-chebby <chebetf034@gmail.com>
2024-04-04 22:22:06 +0300

    My portfolio

---
+++ portfolio/src/Data/Pages/HeroSection.jsx
@@ -0,0 +1,13 @@
+export default function HeroSection(){
+    return(
+        <div id="HeroSection" className="Hero--section">
+         <div className="Hero--section--box">
+             <div>
+                 
+             </div>
+
+         </div>
+
+        </div>
+    );
+}
