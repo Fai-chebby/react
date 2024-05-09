7d5e56eec526b136351bb756b53b8eba811d88a9
Fai-chebby <chebetf034@gmail.com>
2024-05-09 14:17:59 +0300

    portfolio

--- portfolio/src/Data/Pages/HeroSection.jsx
+++ portfolio/src/Data/Pages/HeroSection.jsx
@@ -1,14 +1,16 @@
+import React from "react";
+import MePic from "../pics/me .jpeg"
+import "./HeroSection.css";
 export default function HeroSection(){
     return(
         <div id="HeroSection" className="Hero--section">
          <div className="Hero--section--box">
              <div className="Hero--section--content">
                  <p className="title"> Hey 🤗 I'm Chebby</p>
-                 <h4 className="titlle--sub"> Loved & Graced</h4>
-                 <h1 className="tittle--des">
+                 <h4 className="title--sub"> Loved & Graced</h4>
+                 <h1 className="title--des">
                      <span className="color"> UI & UX</span>{""}<br/>
-                     <span>Graphic</span>
-                     Developer
+                     <span>Graphic</span> <br/><span>Developer</span>
 
                  </h1>
                  <p className="desc">Learning is part of me.<span> But coding is fun👩‍💻</span></p>
@@ -17,6 +19,9 @@ export default function HeroSection(){
              <button className="btn btn-primary">Be In Touch</button>
 
          </div>
+            <div className="image">
+                <img  className="pic"  src={MePic} width="600px" height="600px" alt="Me coding"/>
+            </div>
 
         </div>
     );
