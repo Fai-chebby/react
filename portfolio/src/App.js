51a6632d17cb68048a99d14e404ee27b3b744741
Fai-chebby <chebetf034@gmail.com>
2024-05-10 19:27:38 +0300

    My portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import HeroSection from "./Data/Pages/HeroSection";
 import {Route,Routes} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-<HeroSection/>
-    </div>
+      <Routes>
+   <Route path="/" element={<HeroSection/>}/>
+
+
+      </Routes>
   );
 }
 
